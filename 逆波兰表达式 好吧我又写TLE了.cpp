f9e47176e4c849a6e7c8623eb5b0369c048f8257
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-04-04 12:24:24 +0800

    TLE

---
+++ 逆波兰表达式 好吧我又写TLE了.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cmath>
+#include <stack>
+#define N 10000
+#define M 30
+using namespace std;
+int Scan();
+char a[N][M];
+void Operation(int i);
+int JudgeOperator(int n);
+stack<double> num;
+int main()	{
+	int i=0;
+	i=Scan();
+	Operation(i);
+	double ans=num.top(); 
+	printf("%.6f\n",ans);
+	return 0;
+}
+int Scan()	{
+	int i=0;
+	while (1)	{
+		scanf("%s",a[i]);
+		if (getchar()=='\n')	break;
+		i++;
+	}
+	return i;
+}
+void Operation(int n)	{
+	if (!JudgeOperator(n))
+	{
+		double temp=atof(a[n]);
+		num.push(temp);
+	}
+	if (!n)	return;
+	Operation(--n);
+}
+int JudgeOperator(int n)	{
+	switch(a[n][0])	{
+		case '+':	{
+			double a=num.top();
+			num.pop();
+			double b=num.top();
+			num.pop();
+			double temp=b+a;
+			num.push(temp);
+			return 1;
+			break;
+		}
+		case '-':	{
+			double a=num.top();
+			num.pop();
+			double b=num.top();
+			num.pop();
+			double temp=b-a;
+			num.push(temp);
+			return 1;
+			break;
+		}
+		case '*':	{
+			double a=num.top();
+			num.pop();
+			double b=num.top();
+			num.pop();
+			double temp=b*a;
+			num.push(temp);
+			return 1;
+			break;
+		}
+		case '/':	{
+			double a=num.top();
+			num.pop();
+			double b=num.top();
+			num.pop();
+			double temp=b/a;
+			num.push(temp);
+			return 1;
+			break;
+		}
+		default:return 0;break;
+	}
+}
