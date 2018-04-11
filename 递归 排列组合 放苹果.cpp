2507f168b5a22af6d8965b5811c255617d826873
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-04-11 22:17:50 +0800

    。

---
+++ 递归 排列组合 放苹果.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int combi(int m,int n);
+int main()	{
+	int t;
+	scanf("%d",&t);
+	while (t--)	{
+		scanf("%d %d",&m,&n);
+		printf("%d\n",combi(m,n));
+	}
+	return 0;
+}
+
+int combi(int m,int n)	{
+	
+}
